daeb53e689d397769131b20d3ae7875bd0782953
crcordova <cristobalcordova@hotmail.com>
2022-11-05 13:28:55 -0300

    add form with node

--- hello_world.js
+++ hello_world.js
@@ -1,9 +1,21 @@
 const express = require('express')
+var parseUrl = require('body-parser')
 const app = express()
 const port = 3000
 
+let encodeUrl = parseUrl.urlencoded({ extended: false })
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.sendFile(__dirname + '/form.html')
+})
+
+app.post('/', encodeUrl, (req, res) => {
+  console.log('Form request:', req.body)
+  res.sendStatus(200)
 })
 
 app.listen(port, () => {
